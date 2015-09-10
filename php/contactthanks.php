5a3c23f86dd74f2698f3a2ddba86935d89df329e
KMSkelton <kmskelton@outlook.com>
2015-09-09 18:04:00 -0700

    contact form works, added labels to icons, charts work, added about me

---
+++ php/contactthanks.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>A Nice & Simple Contact Form</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
+		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>
+
+
+		<br />
+		<br />
+
+
+		<h1>Your message has been sent!</h1>
+		<h2>If you do not receive a response within 72 hours, please contact<br /> kristopher at kmskelton dot com.</h2>
+		<br />
+
+		<p><a href="../index.html">Back to KMSKELTON home</a></p>
+
+	</div>
+
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+		_uacct = "UA-68528-29";
+		urchinTracker();
+	</script>
+
+</body>
+
+</html>
