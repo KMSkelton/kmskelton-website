be7c91d2763123760c18b7a2bf7b137cc52a3db5
KMSkelton <kmskelton@outlook.com>
2016-05-27 09:47:41 -0700

    updated projects

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   }, 3500);
   setTimeout(function() {
     slideMe();
-    $("#headerText").append("<br /><h6 align='center' style='display:inline-block; font-size:0.5em; margin-top: 5em'>click me</h6>"); //yes this is hacky, and I don't particularly like it, but the CSS has already loaded and a page reload would restart the timers.
+    $("#headerText").append("<br /><h6 align='center' style='display:inline-block; color:yellow; font-size:0.5em; margin-top: 5em; cursor: pointer;'>click me</h6>"); //yes this is hacky, and I don't particularly like it, but the CSS has already loaded and a page reload would restart the timers.
   }, 6500);
   $("#RWDtext").click(function() {
     slideMe();
