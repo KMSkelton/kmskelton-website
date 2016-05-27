6ca37210522f117dcbab2d64b149ef0fc1137198
KMSkelton <kmskelton@outlook.com>
2016-05-27 16:18:56 -0700

    added 'download resume' button, changed fonts

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
   }
   setTimeout(function() {
     slideMe();
-  }, 3500);
+  }, 1500);
   setTimeout(function() {
     slideMe();
     $("#headerText").append("<br /><h6 align='center' style='display:inline-block; color:yellow; font-size:0.5em; margin-top: 5em; cursor: pointer;'>click me</h6>"); //yes this is hacky, and I don't particularly like it, but the CSS has already loaded and a page reload would restart the timers.
-  }, 6500);
+  }, 3500);
   $("#RWDtext").click(function() {
     slideMe();
   });
