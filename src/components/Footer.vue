6fcfadd6a27977cd833fb06e0a2dcbb87ec735b8
KMSkelton <kmskelton@outlook.com>
2022-03-24 18:15:25 -0700

    fix icomoon fonts

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,28 +2,28 @@
   <footer>
     <a href="./contact">
         <span aria-hidden="true" class="icon-envelop icon">
-          <span class="imageDescription">Contact and Resume</span>
+          <span>Contact and Resume</span>
         </span>
     </a>
       <a href="https://github.com/KMSkelton" target="_blank">
         <span class="icon-github4 icon">
-          <span class="imageDescription">Github</span>
+          <span>Github</span>
         </span>
       </a>
       <a href="https://kmskelton.wordpress.com" target="_blank">
         <span class="icon-wordpress icon">
-          <span class="imageDescription">Wordpress</span>
+          <span>Wordpress</span>
         </span>
       </a>
       <a href="https://www.linkedin.com/in/kristopherskelton" target="_blank">
         <span class="icon-linkedin icon">
-          <span class="imageDescription">LinkedIn</span>
+          <span>LinkedIn</span>
         </span>
     </a>
   </footer>
 </template>
 
-<style scoped>
+<style>
   footer {
     font-size: 120%;
     background-color: #3D3B3D;
